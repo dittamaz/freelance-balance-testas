a04c3f787940a994af9d7667bf2325828bfbb2ac
dittamaz <129725385+dittamaz@users.noreply.github.com>
2023-05-29 09:28:35 +0300

    Mėnesių pavadinimai

--- js/data.js
+++ js/data.js
@@ -1,14 +1,14 @@
-const account = [
-    { month: 10, income: 150 },
-    { month: 1, income: 200, expense: 50 },
-    { month: 3, income: 450, expense: 200 },
-    { month: 7, expense: 50 },
-    { month: 2, income: 50 },
-    { month: 5, income: 150, expense: 50 },
-    { month: 9, income: 700, expense: 400 },
-    { month: 4, income: 10, expense: 300 },
-    { month: 6, expense: 50 },
-    { month: 11, income: 150, expense: 30 },
-    { month: 8, income: 450, expense: 120 },
-    { month: 12, expense: 200 }
+const account = [Sausis, Vasaris, Kovas, Balandis, Gegužė, Birželis, Liepa, Rūgpjūtis, Rugsėjis, Spalis, Lapkritis, Gruodis
+    { Spalis: 10, income: 150 },
+    { Sausis: 1, income: 200, expense: 50 },
+    { Kovas: 3, income: 450, expense: 200 },
+    { Liepa: 7, expense: 50 },
+    { Vasaris: 2, income: 50 },
+    { Gegužė: 5, income: 150, expense: 50 },
+    { Rugsėjis: 9, income: 700, expense: 400 },
+    { Balandis: 4, income: 10, expense: 300 },
+    { Birželis: 6, expense: 50 },
+    { Lapkritis: 11, income: 150, expense: 30 },
+    { Rūgpjūtis: 8, income: 450, expense: 120 },
+    { Gruodis: 12, expense: 200 }
 ];
