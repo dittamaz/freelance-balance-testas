0db140fa9a5201c0ce0737685b513b6370326bee
dittamaz <129725385+dittamaz@users.noreply.github.com>
2023-05-29 09:39:04 +0300

    Sugeneruota

--- js/data.js
+++ js/data.js
@@ -1,14 +1,15 @@
 const account = [Sausis, Vasaris, Kovas, Balandis, Gegužė, Birželis, Liepa, Rūgpjūtis, Rugsėjis, Spalis, Lapkritis, Gruodis
-    { Spalis: 10, income: 150 },
-    { Sausis: 1, income: 200, expense: 50 },
-    { Kovas: 3, income: 450, expense: 200 },
-    { Liepa: 7, expense: 50 },
-    { Vasaris: 2, income: 50 },
-    { Gegužė: 5, income: 150, expense: 50 },
-    { Rugsėjis: 9, income: 700, expense: 400 },
-    { Balandis: 4, income: 10, expense: 300 },
-    { Birželis: 6, expense: 50 },
-    { Lapkritis: 11, income: 150, expense: 30 },
-    { Rūgpjūtis: 8, income: 450, expense: 120 },
-    { Gruodis: 12, expense: 200 }
+    { [9]: 10, income: 150 },
+    { [0]: 1, income: 200, expense: 50 },
+    { [2]: 3, income: 450, expense: 200 },
+    { [6]: 7, expense: 50 },
+    { [1]: 2, income: 50 },
+    { [4]: 5, income: 150, expense: 50 },
+    { [8]: 9, income: 700, expense: 400 },
+    { [3]: 4, income: 10, expense: 300 },
+    { [5]: 6, expense: 50 },
+    { [10]: 11, income: 150, expense: 30 },
+    { [7]: 8, income: 450, expense: 120 },
+    { [11]: 12, expense: 200 }
 ];
+console.log();
